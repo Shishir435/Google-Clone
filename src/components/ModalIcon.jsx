9be34ba784418b7996442107b59dd7ae64de3746
Shishir <shishirchaurasiya435@gmail.com>
2023-03-30 20:00:59 +0530

    modal icons added

--- src/components/ModalIcon.jsx
+++ src/components/ModalIcon.jsx
@@ -3,17 +3,39 @@
 import './ModalIcon.css'
 
 import React from 'react'
-import modalDetail from '../data/modalData'
+import {modalDetailOne, modalDetailTwo} from '../data/modalData'
+
 
 const ModalIcon = () => {
   return (
+    <div>
     <div className='modal__container'>
-      {modalDetail.map((item) => (
-        <div className='modal__icon'>
-          <img src={item.img} alt={item.name} />
-          <p>{item.name}</p>
-        </div>
-      ))}</div>
+
+      {modalDetailOne.map((item) => (
+        <a href={item.url}>
+          <div key={item.id} className='modal__icon'>
+            <img height={40} width={40} src={item.img} alt={item.name} />
+            <p>{item.name}</p>
+          </div>
+         
+          </a>
+      ))}
+      </div>
+      <hr style={{borderBottom: '1px solid  rgb(213, 211, 211)', widows: '100%', margin: '4rem 0 1rem 0'}} />
+    <div className='modal__container'>
+
+      {modalDetailTwo.map((item) => (
+        <a href={item.url}>
+          <div key={item.id} className='modal__icon'>
+            <img height={40} width={40} src={item.img} alt={item.name} />
+            <p>{item.name}</p>
+          </div>
+         
+          </a>
+      ))}
+      </div>
+      
+      </div>
   )
 }
 
--- src/data/modalData.js
+++ src/data/modalData.js
@@ -1,88 +1,208 @@
+export const modalDetailOne = [
+ 
+  {  
+    id: '1',
+    name: "Account",
+    img: "/assets/account-modified.png",
+    url: "https://myaccount.google.com/",
+  },
+  {  
+    id: '2',
+    name: "Search",
+    img: "/assets/modal-icons/icons8-google-96.svg",
+    url: "https://www.google.com/?authuser=0",
+  },
+  { 
+    id: '3',
+    name: "Business",
+    img: "/assets/modal-icons/google-my-business-logo-1.svg",
+    url: "https://www.google.com/business/?ppsrc=GPDA2&gmbsrc=ww-ww-ot-gs-z-gmb-l-z-h~z-ogb-u",
+  },
+  { 
+    id: '4',
+    name: "Maps",
+    img: "/assets/modal-icons/icons8-google-maps-96.svg",
+    url: "https://www.google.com/maps",
+  },
+  { 
+    id: '5',
+    name: "YouTube",
+    img: "/assets/modal-icons/icons8-youtube-96.svg",
+    url: "https://youtube.com/",
+  },
+  { 
+    id: '6',
+    name: "Play",
+    img: "../../public/assets/modal-icons/google-play-5.svg",
+    url: "https://play.google.com/store/games",
+  },
+  { 
+    id: '7',
+    name: "News",
+    img: "../../public/assets/modal-icons/google-news-2.svg",
+    url: "https://news.google.com/home",
+  },
+  { 
+    id: '8',
+    name: "Gmail",
+    img: "../../public/assets/modal-icons/official-gmail-icon-2020-.svg",
+    url: "https://mail.google.com/mail/",
+  },
+  { 
+    id: '9',
+    name: "Meet",
+    img: "../../public/assets/modal-icons/icons8-google-meet-96.svg",
+    url: "https://meet.google.com/",
+  },
+  { 
+    id: '10',
+    name: "Chat",
+    img: "../../public/assets/modal-icons/Google_Chat_Logo.svg",
+    url: "https://mail.google.com/chat/u/0/#chat/welcome",
+  },
+  { 
+    id: '11',
+    name: "Contact",
+    img: "../../public/assets/modal-icons/icons8-google-contacts-96.svg",
+    url: "https://contacts.google.com/",
+  },
+  { 
+    id: '12',
+    name: "Drive",
+    img: "../../public/assets/modal-icons/icons8-google-drive-96.svg",
+    url: "https://drive.google.com/",
+  },
+  { 
+    id: '13',
+    name: "Calender",
+    img: "../../public/assets/modal-icons/icons8-google-calendar-96.svg",
+    url: "https://calendar.google.com/calendar/u/0/r",
+  },
+  { 
+    id: '14',
+    name: "Translate",
+    img: "../../public/assets/modal-icons/icons8-google-translate-96.svg",
+    url: "https://translate.google.com/",
+  },
+  { 
+    id: '15',
+    name: "Photos",
+    img: "../../public/assets/modal-icons/icons8-google-photos-96.svg",
+    url: "https://photos.google.com/",
+  },
+  { 
+    id: '16',
+    name: "My Ad Center",
+    img: "../../public/assets/modal-icons/my-ad-center-seeklogo.com.svg",
+    url: "https://myadcenter.google.com/",
+  },
+  { 
+    id: '17',
+    name: "Shopping",
+    img: "../../public/assets/modal-icons/google-shopping.svg",
+    url: "https://shopping.google.com/",
+  },
+  
+];
 
+export  const modalDetailTwo=[
+  { 
+    id: '31',
+    name: "Finance",
+    img: "../../public/assets/modal-icons/google-finance.svg",
+    url: "https://www.google.com/finance/",
+  },
+  { 
+    id: '32',
+    name: "Docs",
+    img: "../../public/assets/modal-icons/icons8-google-docs-96.svg",
+    url: "https://docs.google.com/document/u/0/",
+  },
+  { 
+    id: '33',
+    name: "Sheet",
+    img: "../../public/assets/modal-icons/icons8-google-sheets-96.svg",
+    url: "https://docs.google.com/spreadsheets/u/0/",
+  },
+  { 
+    id: '34',
+    name: "Slide",
+    img: "../../public/assets/modal-icons/icons8-google-slides-96.svg",
+    url: "https://docs.google.com/presentation/u/0/",
+  },
+  { 
+    id: '46',
+    name: "Form",
+    img: "../../public/assets/modal-icons/Google_Forms_Logo.svg",
+    url: "https://docs.google.com/forms/u/0/",
+  },
+  { 
+    id: '35',
+    name: "Books",
+    img: "../../public/assets/modal-icons/icons8-google-books-96.svg",
+    url: "https://books.google.com/?authuser=0",
+  },
+  { 
+    id: '36',
+    name: "Blogger",
+    img: "../../public/assets/modal-icons/blogger-1.svg",
+    url: "https://www.blogger.com/about/?bpli=1",
+  },
+  { 
+    id: '37',
+    name: "Hangout",
+    img: "../../public/assets/modal-icons/icons8-google-hangouts-chat-96.svg",
+    url: "https://mail.google.com/chat/u/0/#chat/welcome",
+  },
+  { 
+    id: '38',
+    name: "Keep",
+    img: "../../public/assets/modal-icons/icons8-google-keep-96.svg",
+    url: "https://keep.google.com/?authuser=0",
+  },
+  { 
+    id: '39',
+    name: "Classroom",
+    img: "../../public/assets/modal-icons/icons8-google-classroom-96.svg",
+    url: "https://classroom.google.com/",
+  },
+  { 
+    id: '40',
+    name: "Earth",
+    img: "../../public/assets/modal-icons/icons8-google-earth-96.svg",
+    url: "https://earth.google.com/web/?authuser=0",
+  },
+  { 
+    id: '41',
+    name: "Collection",
+    img: "../../public/assets/modal-icons/google-collections-icon.svg",
+    url: "https://www.google.com/save?authuser=0",
+  },
+  { 
+    id: '42',
+    name: "Ads",
+    img: "../../public/assets/modal-icons/icons8-google-ads-96.svg",
+    url: "https://ads.google.com/home/",
+  },
+  { 
+    id: '43',
+    name: "Podcast",
+    img: "../../public/assets/modal-icons/icons8-google-podcasts-96.svg",
+    url: "https://podcasts.google.com/",
+  },
+  { 
+    id: '44',
+    name: "One",
+    img: "../../public/assets/modal-icons/google-one.svg",
+    url: "https://one.google.com/",
+  },
+  { 
+    id: '45',
+    name: "Travel",
+    img: "../../public/assets/modal-icons/google-travel.svg",
+    url: "https://www.google.com/travel/",
+  },
+ 
+];
 
-export const modalDetail =[
-
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-{
-    name: 'Account',
-    img: '/assets/account.jpg',
-    url: ''
-},
-
-
-
-
-
-]
-
-
-export default modalDetail;
+// export { modalDetailOne, modalDetailTwo };
